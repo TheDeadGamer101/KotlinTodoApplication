cf0a4aa45f6cea2aee01d7d84f73cd30db4f6b39
MK <thedeadgamer101@gmail.com>
2022-07-30 19:24:50 +0300

    Added main stylization and task stylization

--- app/src/main/java/com/notifications/todoapplication/MainActivity.kt
+++ app/src/main/java/com/notifications/todoapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.notifications.todoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private var todos : ArrayList<Int> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.addTaskButton).setOnClickListener {
+            findViewById<LinearLayout>(R.id.addTaskContainer).visibility = View.VISIBLE
+        }
+
+        findViewById<Button>(R.id.addTaskCancelButton).setOnClickListener {
+            findViewById<LinearLayout>(R.id.addTaskContainer).visibility = View.GONE
+        }
     }
 }
