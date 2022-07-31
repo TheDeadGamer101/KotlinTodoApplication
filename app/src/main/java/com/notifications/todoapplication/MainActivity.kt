d380bdca9c89e25bc7c625f159622300b475c2e4
MK <thedeadgamer101@gmail.com>
2022-07-31 13:38:23 +0300

    Able to delete and add tasks

--- app/src/main/java/com/notifications/todoapplication/MainActivity.kt
+++ app/src/main/java/com/notifications/todoapplication/MainActivity.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -41,16 +40,56 @@ class MainActivity : AppCompatActivity() {
             findViewById<LinearLayout>(R.id.addTaskContainer).visibility = View.GONE
         }
 
+        // on click listener for task add confirmation
+        findViewById<Button>(R.id.addTaskConfirmButton).setOnClickListener {
+            findViewById<LinearLayout>(R.id.addTaskContainer).visibility = View.GONE
 
+            createNewTask()
+
+            findViewById<EditText>(R.id.taskNameField).hideKeyboard()
+
+            findViewById<EditText>(R.id.taskNameField).setText("")
+        }
+
+        // on click listener for deletion of all tasks
+        findViewById<Button>(R.id.removeAllTasksButton).setOnClickListener {
+            deleteAllTasks()
+        }
+
+    }
+
+    // delete all current tasks
+    private fun deleteAllTasks(){
+        todos.clear()
+
+        populateTaskList()
+    }
+
+    // add new task to the list
+    private fun createNewTask(){
+        var text = findViewById<EditText>(R.id.taskNameField).text.toString()
+        todos.add(Todo(text,false))
+
+        populateTaskList()
     }
 
     // Populate the task list
     private fun populateTaskList(){
+        taskList?.removeAllViews()
         todos.forEach { task ->
             run{
                 val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
                 val rowView: View = inflater.inflate(R.layout.single_task, null)
+                rowView.findViewById<TextView>(R.id.singleTaskName).text = task.getText()
+                rowView.findViewById<CheckBox>(R.id.singleTaskIsDone).isChecked = task.getIsDone()
                 taskList!!.addView(rowView)
         } }
     }
+
+    // function for hiding keyboard in a single view
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
 }
